35f455826c54a45a3126d961308679a3da1f00c3
AlejandoSJ9 <alejandro.salas090603@gmail.com>
2025-03-07 17:33:36 -0500

    feat: Add DB connection using .evn and postgres driver

--- src/main/java/org/alejo/config/DataBaseConnection.java
+++ src/main/java/org/alejo/config/DataBaseConnection.java
@@ -1,2 +1,37 @@
-package org.alejo.config;public class DataBaseConnection {
+package org.alejo.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            String url = System.getenv("DB_URL_TIENDA");
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
+
+            if (url == null || user == null || password == null) {
+                throw new IllegalStateException("Las variables de entorno no están configuradas.");
+            }
+
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("✅ Conexión exitosa a la base de datos");
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("🔴 Conexión cerrada");
+            }
+        } catch (SQLException e) {
+            System.err.println("❌ Error al cerrar la conexión: " + e.getMessage());
+        }
+    }
 }
+
