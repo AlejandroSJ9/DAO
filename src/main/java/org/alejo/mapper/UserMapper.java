10b3bfa975fbd4762e49bf3069abac13a50d3c81
ALEJO\AlejandroSJ99 <alejandro.salas090603@gmail.com>
2025-03-07 17:52:06 -0500

    feat: Add mapper for each model

---
+++ src/main/java/org/alejo/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package org.alejo.mapper;
+
+import org.alejo.dto.UserDTO;
+import org.alejo.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO toDTO(User user);
+    User toEntity(UserDTO userDTO);
+}
